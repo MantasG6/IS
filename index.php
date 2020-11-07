85d51bcf3924a8846905d504d6fcf2dd6c469911
Gricius Mantas <mangri5@ktu.lt>
2020-11-07 17:06:48 +0200

    Gamybos valdymo posistemes patvarkymas

--- index.php
+++ index.php
@@ -25,18 +25,16 @@
     </style>
 </head>
 <body>
-    <ul class="list-unstyled">
-        <h1 class="header">Fabriko informacinė sistema</h1>
-        <div class="container">
-            <ul class="list-unstyled">
-                <li><a href="./Gamybos-valdymas/index.php" class="btn btn-warning posistemes">Gamybos valdymas</a></li>
-                <li><a href="./Fasavimo-procesu-valdymas/index.php" class="btn btn-warning posistemes">Fasavimo procesų valdymas</a></li>
-                <li><a href="./Personalo-administravimas/index.php" class="btn btn-warning posistemes">Personalo administravimas</a></li>
-                <li><a href="./Klientu-valdymas/index.php" class="btn btn-warning posistemes">Klientų valdymas</a></li>
-                <li><a href="./Logistikos-procesu-valdymas/index.php" class="btn btn-warning posistemes">Logistikos procesų valdymas</a></li>
-            </ul>
-        </div>
-    </ul> 
+    <h1 class="header">Fabriko informacinė sistema</h1>
+    <div class="container">
+        <ul class="list-unstyled">
+            <li><a href="./Gamybos-valdymas/index.php" class="btn btn-warning posistemes">Gamybos valdymas</a></li>
+            <li><a href="./Fasavimo-procesu-valdymas/index.php" class="btn btn-warning posistemes">Fasavimo procesų valdymas</a></li>
+            <li><a href="./Personalo-administravimas/index.php" class="btn btn-warning posistemes">Personalo administravimas</a></li>
+            <li><a href="./Klientu-valdymas/index.php" class="btn btn-warning posistemes">Klientų valdymas</a></li>
+            <li><a href="./Logistikos-procesu-valdymas/index.php" class="btn btn-warning posistemes">Logistikos procesų valdymas</a></li>
+        </ul>
+    </div>
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
