18cccba53f40f00ce4f43c92916c348149760124
dov11s <64472321+dov11s@users.noreply.github.com>
2020-11-10 11:29:03 +0200

    Atnaujinimas
    Logistikos skyriaus pridejimas

--- Logistikos-procesu-valdymas/Kroviniu-stebejimas.php
+++ Logistikos-procesu-valdymas/Kroviniu-stebejimas.php
@@ -4,7 +4,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Logistikos procesų valdymas</title>
+    <title>Krovinių stebėjimas</title>
     <style>
         html,
         body {
@@ -25,16 +25,16 @@
     </style>
 </head>
 <body>
-    <a href="../index.php" class="btn btn-warning">Grįžti</a>
+    <a href="index.php" class="btn btn-warning">Grįžti</a>
 
-    <h2 class="header">Logistikos procesų valdymas</h2>
+    <h3 class="header">Krovinių stebėjimas</h3>
     <div class="container">
         <ul class="list-unstyled">
-            <li><a href="Kroviniu-stebejimas.php" class="btn btn-warning funkcijos">Krovinių stebėjimas</a></li>
-            <li><a href="Važtaraščiai.php" class="btn btn-warning funkcijos">Važtaraščiū peržiūra</a></li>
+            <li><a href="#" class="btn btn-warning funkcijos">Atvykstantys kroviniai</a></li>
+            <li><a href="#" class="btn btn-warning funkcijos">Išvykstantys kroviniai</a></li>
+            <li><a href="#" class="btn btn-warning funkcijos">Krovinio redagavimas</a></li>
         </ul>
     </div>
-
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 </body>
